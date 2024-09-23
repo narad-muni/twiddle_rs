a31cf346e3e3c55179806dcde30f8bf9282fefc3
Motabhai <>
2024-09-23 19:29:16 +0530

    Remove float

--- src/lib.rs
+++ src/lib.rs
@@ -52,7 +52,6 @@ fn is_numeric_type(ty: &Type) -> bool {
             match type_segment.ident.to_string().as_str() {
                 "i8" | "i16" | "i32" | "i64" | "i128" | "isize" |
                 "u8" | "u16" | "u32" | "u64" | "u128" | "usize" |
-                "f32" | "f64" => true,
                 _ => false,
             }
         }
