452744f5d1585530d7162860aeedfebeba4bdeaa
Motabhai <>
2024-09-24 12:33:31 +0530

    Fix error

--- src/lib.rs
+++ src/lib.rs
@@ -51,7 +51,7 @@ fn is_numeric_type(ty: &Type) -> bool {
             let type_segment = type_path.path.segments.last().unwrap();
             match type_segment.ident.to_string().as_str() {
                 "i8" | "i16" | "i32" | "i64" | "i128" | "isize" |
-                "u8" | "u16" | "u32" | "u64" | "u128" | "usize" |
+                "u8" | "u16" | "u32" | "u64" | "u128" | "usize" => true,
                 _ => false,
             }
         }
